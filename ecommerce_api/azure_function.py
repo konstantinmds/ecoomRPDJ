3f4e9c6ddf1a2139b8fcb3249db147728f5cbbf6
konstantinmds <kosta.ar92@gmail.com>
2021-06-12 23:12:07 +0200

    Update azure_function.py

--- ecommerce_api/azure_function.py
+++ ecommerce_api/azure_function.py
@@ -3,13 +3,11 @@ from storages.backends.azure_storage import AzureStorage
 
 class AzureMediaStorage(AzureStorage):
   account_name = 'djangoaccountstorage1'
-  account_key = "YskH4SZfThHBz4ia9DPefjOINfzDHMjinlr/t1eX0WNWcKVjiOh7XeF3rLkrWzT7bD22FCS2qbUD4MIaRiSh4Q=="
   azure_container = 'media'
   expiration_secs = None
 
 
 class AzureStaticStorage(AzureStorage):
   account_name = 'djangoaccountstorage1'
-  account_key = "YskH4SZfThHBz4ia9DPefjOINfzDHMjinlr/t1eX0WNWcKVjiOh7XeF3rLkrWzT7bD22FCS2qbUD4MIaRiSh4Q=="
-  azure_container = 'static'
+    azure_container = 'static'
   expiration_secs = None
